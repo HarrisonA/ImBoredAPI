d2ef631761da21a9513796e5894ef48ab461c186
HarrisonA <radiov@gmail.com>
2016-08-31 12:25:37 -0700

    added test to verify format of JSON response.  MVP solution

--- spec/requests/cities_spec.rb
+++ spec/requests/cities_spec.rb
@@ -1,3 +1,5 @@
+# rubocop:disable all
+
 require 'rails_helper'
 
 RSpec.describe City, type: :request do
@@ -10,9 +12,14 @@ RSpec.describe City, type: :request do
 
        expect(response.status).to eq 200
 
-       #puts "the repsonse: #{response.body}"
+       # puts "\n\nthe response: #{response.body}\n\n\n\n"
 
+       # parse JSON into Ruby hash
+       # Ex: From {"data":[{"id":"1", to {"data"=>[{"id"=>"1"
        body = JSON.parse(response.body)
+
+       #puts "ruby hash #{body['data']}"
+
        city_names = body['data'].map{|city| city['attributes']['name'] }
        expect(city_names).to match_array(['New York', 'Lagos'])
      end
@@ -31,4 +38,43 @@ RSpec.describe City, type: :request do
       expect(city_name) == 'Sao Paulo'
     end
   end
-end
+
+   describe "Correctly serializes JSON response from /cities" do
+     it "format is correct" do
+       create(:city, name: 'New York')
+       create(:city, name: 'Lagos')
+
+       get '/cities'
+
+       expect(response.status).to eq 200
+
+       body = JSON.parse(response.body)
+
+       # There's got to be a better way to check the existence of keys!!!
+
+       expect(body['data']).not_to be_nil
+       expect(body['data'][0]['id']).not_to be_nil
+       expect(body['data'][0]['type']).to eq "cities"
+       expect(body['data'][0]['attributes']['name']).not_to be_nil
+       expect(body['data'][0]['relationships']['travels']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['movies']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['restaurants']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['parks']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['bars']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['sports']['data']).not_to be_nil
+       expect(body['data'][0]['relationships']['families']['data']).not_to be_nil
+
+       # Make sure the second city is in the same format
+       expect(body['data'][1]['id']).not_to be_nil
+       expect(body['data'][1]['type']).to eq "cities"
+       expect(body['data'][1]['attributes']['name']).not_to be_nil
+       expect(body['data'][1]['relationships']['travels']['data']).not_to be_nil
+
+       # Small error with this loop (key variable)  - Ruby syntaxviolation?
+      #  body['data'][1]['relationships'].each do |key|
+      #    expect(body['data'][1]['relationships'][key]['data']).not_to be_nil
+      #  end
+
+     end
+   end
+ end
